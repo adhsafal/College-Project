721353970a81a2cc01317ec430e5b0436cab21ae
Safal <mr.safaladh@gmail.com>
2022-03-08 20:12:39 +0545

    minor change

--- stylehub/src/Components/Home/Home.js
+++ stylehub/src/Components/Home/Home.js
@@ -52,7 +52,7 @@ const Home = () => {
                 </div>
                 <div className="row intro-details">
                     <div className="col-lg-6 col-md-6">
-                        <h1>STYLE <span style={{ fontSize: "6rem" }}> <br />HUB</span></h1>
+                        <h1 className='style'>STYLE <span style={{ fontSize: "6rem" }}> <br />HUB</span></h1>
                         <hr />
                         <p>Design and create your own T-shirts, hoodies and many more with exciting color options.</p>
                         <button className="btn btn-lg btn-outline-primary"><Link className="nav-link navbar__links" to="/customize">Start Designing</Link></button>
